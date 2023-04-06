cfd3b40bc0f3abe004b62a3f6284284a31caa023
Soren Childs <sorenchilds@gmail.com>
2023-04-06 14:18:09 -0700

    followed along making the server.

---
+++ My-dream/dreamClone/server.js
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI,
+});
+
+const openai = new OpenAIApi(configuration);
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/dream', async (req, res) =>{
+
+    const prompt = req.body.prompt;
+
+    const aiResponde = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    });
+
+    const image = aiResponse.data.data[0].url;
+    res.send({image});
+
+});
+
+app.listen(8000, () => console.log('make art on http://localhost:8000/dream'));
