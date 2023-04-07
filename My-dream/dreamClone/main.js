77fd091a6e8d6d1e548b3edc87a64b9a4fcff026
Soren Childs <sorenchilds@gmail.com>
2023-04-07 11:00:50 -0700

    Added error handling and disabled button for async

--- My-dream/dreamClone/main.js
+++ My-dream/dreamClone/main.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
+  showSpinner();
   //Creates an object that behaves like a JavaScript Map.
   const data = new FormData(form);
   //creates a promise using the await keyword.
@@ -17,8 +18,30 @@ form.addEventListener('submit', async (e) => {
     }),
   });
 
-  const { image } = await response.json();
+  if (response.ok){
+    const { image } = await response.json();
 
-  const result = document.querySelector('#result');
-  result.innerHTML = `<img src="${image}" width="512" />`;
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`;
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.errror(err);
+  }
+
+  
+
+  hideSpinner();
 });
+
+function showSpinner(){
+  const button = document.querySelector('button');
+  button.disabled = true;
+  button.innerHTML = 'Dreaming <span class="spinner">🧠</span>'
+}
+
+function hideSpinner(){
+  const button = document.querySelector('button');
+  button.disabled = false;
+  button.innerHTML = 'Dream' 
+}
